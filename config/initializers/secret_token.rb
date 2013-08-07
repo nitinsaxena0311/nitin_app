ea54fde1f16d2ac6637e651d60691399feb5efbf
nitinsaxena0311 <nitinsaxena0311@gmail.com>
2013-08-07 23:35:23 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NitinApp::Application.config.secret_key_base = 'dee6ce169bc2cbd386cc05ff69ec91ea497f2271931702bc4366e19a5b4147533f8e65ca21055da11fe73d6d002dee32272db15814b862d126ed3adbbdd77b28'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NitinApp::Application.config.session_store :cookie_store, key: '_nitin_app_session'
